44dfabc9069ccff684ccec7ff0f441c6046bfacb
Ivan Panchenko <arigatory@gmail.com>
2020-11-04 15:59:44 +0300

    before setting up SQLite

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import pdb
 
 app = Flask(__name__)
 
@@ -8,5 +9,9 @@ def home():
     return render_template("home.html")
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/item/new', methods=["GET", "POST"])
+def new_item():
+    if request.method == "POST":
+        print("Form data")
+        return  redirect(url_for('home'))
+    return render_template("new_item.html")
