54f03ccf2f6f8c3e6f72f095e62156dcdf12d910
Ivan Panchenko <arigatory@gmail.com>
2020-11-05 03:03:42 +0300

    not working

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for, g
 import pdb
 
 app = Flask(__name__)
@@ -11,7 +12,36 @@ def home():
 
 @app.route('/item/new', methods=["GET", "POST"])
 def new_item():
+    conn = get_db()
+    c = conn.cursor()
+
     if request.method == "POST":
-        print("Form data")
-        return  redirect(url_for('home'))
+        c.execute("""INSERT INTO items
+        (title, description, price, image, category_id, subcategory_id)
+        VALUES(?,?,?,?,?,?)""",
+                  (
+                      request.form.get("title"),
+                      request.form.get("description"),
+                      float(request.form.get("price")),
+                      "",
+                      1,
+                      1
+                  )
+        )
+        conn.commit()
+        return redirect(url_for('home'))
     return render_template("new_item.html")
+
+
+def get_db():
+    db = getattr(g, "_database", None)
+    if db is None:
+        g._database = sqlite3.connect("db/globomantics.db")
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, "_database", None)
+    if db is not None:
+        db.close()
